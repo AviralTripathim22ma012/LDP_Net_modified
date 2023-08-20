ce85a50eea0f164034e2c03a146fe7eb6c43f79b
AviralTripathim22ma012 <126051960+AviralTripathim22ma012@users.noreply.github.com>
2023-08-20 16:21:12 +0530

    Update test_dataset.py

--- test_dataset.py
+++ test_dataset.py
@@ -3,6 +3,9 @@ import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
 from PIL import ImageFile
 from PIL import ImageEnhance
+import numpy as np
+import random
+from PIL import ImageFilter
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 identity = lambda x:x
@@ -34,7 +37,7 @@ class SetDataset:
         d = ImageFolder(self.data_path)
         import tqdm
         for i, (data, label) in tqdm.tqdm(enumerate(d)):
-            self.sub_meta[label].append(data)
+            self.sub_meta[label].append(i)
 
     
         self.sub_dataloader = [] 
@@ -42,7 +45,7 @@ class SetDataset:
                                   shuffle = True,
                                   num_workers = 0, #use main thread only or may receive multiple batches
                                   pin_memory = False)        
-        for cl in self.cl_list:
+        for cl in tqdm.tqdm(self.cl_list):
             sub_dataset = SubDataset(self.sub_meta[cl], cl, transform=transform)
             self.sub_dataloader.append(torch.utils.data.DataLoader(sub_dataset, **sub_data_loader_params))
 
