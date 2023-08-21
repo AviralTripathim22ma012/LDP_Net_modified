23f3c8c3802613daaaa1d1eeb37274cea35d8a90
AviralTripathim22ma012 <126051960+AviralTripathim22ma012@users.noreply.github.com>
2023-08-21 14:59:16 +0530

    Update test_dataset.py

--- test_dataset.py
+++ test_dataset.py
@@ -27,7 +27,7 @@ class ImageJitter(object):
     
     
 class SetDataset:
-    def __init__(self, data_path, num_class, batch_size, transform):
+    def __init__(self, data_path, num_class, batch_size):
         self.sub_meta = {}
         self.data_path = data_path
         self.num_class = num_class
@@ -46,7 +46,7 @@ class SetDataset:
                                   num_workers = 0, #use main thread only or may receive multiple batches
                                   pin_memory = False)        
         for cl in tqdm.tqdm(self.cl_list):
-            sub_dataset = SubDataset(self.sub_meta[cl], cl, transform=transform)
+            sub_dataset = SubDataset(self.sub_meta[cl], cl)#, transform=transform)
             self.sub_dataloader.append(torch.utils.data.DataLoader(sub_dataset, **sub_data_loader_params))
 
     def __getitem__(self, i):
@@ -84,40 +84,7 @@ class EpisodicBatchSampler(object):
     def __iter__(self):
         for i in range(self.n_episodes):
             yield torch.randperm(self.n_classes)[:self.n_way]
-    
 
-# class TransformLoader:
-#     def __init__(self, image_size, 
-#                  normalize_param = dict(mean= [0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-#                  jitter_param = dict(Brightness=0.4, Contrast=0.4, Color=0.4)):
-#         self.image_size = image_size
-#         self.normalize_param = normalize_param
-#         self.jitter_param = jitter_param
-    
-#     def parse_transform(self, transform_type):
-#         if transform_type=='ImageJitter':
-#             method = ImageJitter(self.jitter_param)
-#             return method
-#         method = getattr(transforms, transform_type)
-#         if transform_type=='RandomResizedCrop':
-#             return method(self.image_size) 
-#         elif transform_type=='CenterCrop':
-#             return method(self.image_size) 
-#         elif transform_type=='Resize':
-#             return method([int(self.image_size*1.15), int(self.image_size*1.15)])
-#         elif transform_type=='Normalize':
-#             return method(**self.normalize_param )
-#         else:
-#             return method()
-
-#     def get_composed_transform(self, aug = False):
-#         if aug:
-#             transform_list = ['RandomResizedCrop', 'ImageJitter', 'RandomHorizontalFlip', 'ToTensor', 'Normalize']
-#         else:
-#             transform_list = ['Resize','CenterCrop', 'ToTensor', 'Normalize']
-#         transform_funcs = [ self.parse_transform(x) for x in transform_list]
-#         transform = transforms.Compose(transform_funcs)
-#         return transform
 
 class Eposide_DataManager():
     def __init__(self, data_path, num_class, n_way=5, n_support=1, n_query=15, n_eposide=1):        
@@ -132,7 +99,7 @@ class Eposide_DataManager():
 
     def get_data_loader(self, aug): #parameters that would change on train/val set
         # transform = self.trans_loader.get_composed_transform(aug)
-        dataset = SetDataset(self.data_path, self.num_class, self.batch_size, transform)
+        dataset = SetDataset(self.data_path, self.num_class, self.batch_size)
         sampler = EpisodicBatchSampler(len(dataset), self.n_way, self.n_eposide)  
         data_loader_params = dict(batch_sampler=sampler, num_workers=12, pin_memory=True)       
         data_loader = torch.utils.data.DataLoader(dataset, **data_loader_params)
